2ae47d98c054309d71f28e31988dd89cf9870db7
Tikhonov-86 <Sashulka-86@list.ru>
2024-04-19 15:48:25 +0500

    19 April 2024

--- NewsPaper/celery.py
+++ NewsPaper/celery.py
@@ -9,9 +9,9 @@ app.config_from_object('django.conf:settings', namespace='CELERY')
 
 app.autodiscover_tasks()
 
-# app.conf.beat_schedule = {
-#     'action_every_monday_8am': {
-#         'task': 'action',
-#         'schedule': crontab(hour=8, minute=0, day_of_week='monday'),
-#     },
-# }
+app.conf.beat_schedule = {
+    'action_every_monday_8am': {
+        'task': 'action',
+        'schedule': crontab(hour=8, minute=0, day_of_week='monday'),
+    },
+}
--- NewsPaper/tasks.py
+++ NewsPaper/tasks.py
@@ -11,3 +11,4 @@ def printer(N):
     for i in range(N):
         time.sleep(1)
         print(i+1)
+        
